3ca87a80c3ac1e14364d77b641baa3341da9f8d2
asharykin <aleksandr-sharykin@mail.ru>
2024-03-20 03:40:22 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,49 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
+import web.service.UserService;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String getAllUsers(ModelMap model) {
+        List<User> users = userService.getAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam("name") String name, @RequestParam("lastName") String lastName,
+                          @RequestParam("age") int age) {
+        userService.addUser(new User(name, lastName, age));
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users/delete")
+    public String deleteUser(@RequestParam("userId") int userId) {
+        userService.deleteUser(userId);
+        return "redirect:/users";
+    }
+
+    @PostMapping("/users/update")
+    public String updateUser(@RequestParam("userId") int userId, @RequestParam("name") String name,
+                             @RequestParam("lastName") String lastName, @RequestParam("age") String age) {
+        User user = userService.getUserById(userId);
+        user.setName(name.isEmpty() ? user.getName() : name);
+        user.setLastName(lastName.isEmpty() ? user.getLastName() : lastName);
+        user.setAge(age.isEmpty() ? user.getAge() : Integer.parseInt(age));
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void addUser(User user) {
+        userDao.addUser(user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        userDao.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        userDao.deleteUser(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userDao.getAllUsers();
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return userDao.getUserById(id);
+    }
+}
